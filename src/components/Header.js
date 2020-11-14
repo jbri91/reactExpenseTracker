a97b23a59cbf056b7f11011a6153c64c41eb3b86
Johnny Briones <jbrrii91@gmail.com>
2020-11-14 11:25:37 -0500

    jsx style in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 class Header extends React.Component {
     render() {
-        return (<h1> React Expense Tracker</h1>
+        return (<h1 style={{color: '#dc3545', backgroundColor: '#343a40'}} > React Expense Tracker</h1>
         )
         }
 }

5dbcdf249137b52c248366951f3cf041ed484327
Johnny Briones <jbrrii91@gmail.com>
2020-11-18 21:36:47 -0500

    handlechange

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Table from "./components/Table";
-import UserInput from "./UserInput";
+import UserInput from "./components/UserInput";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
